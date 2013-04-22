a9420f73a63abe5f730c2161345a7c6923a3d85e
Matt Field <m.field42@gmail.com>
2013-04-22 17:31:09 +0100

    styles extension and cleanup 	modified:   css/sir-trevor.css 	modified:   js/app.js

--- js/app.js
+++ js/app.js
@@ -2,14 +2,11 @@
 
   var editor = new SirTrevor.Editor({
     el: $('.sir-trevor'),
-    blockTypes: ['Text', 'Quote', 'Tweet', 'Image', 'Ul', 'Ol']
+    blockTypes: ['Text', 'Ul', 'Ol', 'Image', 'Quote', 'Tweet', 'Gallery']
   });
 
   $('form').on('submit', function(e){
-    e.preventDefault();
-
     $('.output').html(editor.$el.val());
-
     return false;
   });
 
